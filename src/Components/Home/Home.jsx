a300a326aec52afbeb4c6c0a178b2979e7e1aa6c
Imran-A-K <imran.k.professional@gmail.com>
2023-04-06 23:34:27 +0600

    made basic router structure and loaded data in home component

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Home = () => {
+    const tshirts = useLoaderData();
+    
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Home;
